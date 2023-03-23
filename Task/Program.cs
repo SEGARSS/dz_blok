471e1d240aab29021cbeb36317ffb239fcbd1961
SEGARSS <124182619+SEGARSS@users.noreply.github.com>
2023-03-23 19:14:17 +0500

    Update Program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,24 +1,23 @@
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
-{
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string array1[5] = { "123", "23", "hello", "world", "res" };
+    string array2[sizeof(array1) / sizeof(array1[0])];
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
+
+    for (int i = 0; i < sizeof(array1) / sizeof(array1[0]); i++) {
+        if (array1[i].length() <= 3) {
+            array2[count] = array1[i];
+            count++;
         }
     }
-}
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
+
+    for (int i = 0; i < sizeof(array2) / sizeof(array2[0]); i++) {
+        cout << array2[i] << " ";
     }
-    Console.WriteLine();
+    cout << endl;
+
+    return 0;
 }
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
